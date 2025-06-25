bae8e0f8e8cd54b0f808f001b9487d48de6b5abf
tariq <rahee.programmer@gmail.com>
2025-06-25 18:52:59 +0500

    first commit

---
+++ src/game/scenes/MainMenu.ts
@@ -0,0 +1,75 @@
+import { GameObjects, Scene } from 'phaser';
+
+import { EventBus } from '../EventBus';
+
+export class MainMenu extends Scene
+{
+    background: GameObjects.Image;
+    logo: GameObjects.Image;
+    title: GameObjects.Text;
+    logoTween: Phaser.Tweens.Tween | null;
+
+    constructor ()
+    {
+        super('MainMenu');
+    }
+    preload(){
+        this.load.image("balloon", "assets/images/airballoon-red.png")
+        this.load.image("background1", "assets/images/location1.png")
+    }
+
+    create ()
+    {
+        this.add.image(670,300, "balloon").setDepth(10)
+        this.add.image(670,1250, "background1").setScale(1.45, 1.45)
+
+
+
+        EventBus.emit('current-scene-ready', this);
+    }
+    
+    changeScene ()
+    {
+        if (this.logoTween)
+        {
+            this.logoTween.stop();
+            this.logoTween = null;
+        }
+
+        this.scene.start('Game');
+    }
+
+    moveLogo (vueCallback: ({ x, y }: { x: number, y: number }) => void)
+    {
+        if (this.logoTween)
+        {
+            if (this.logoTween.isPlaying())
+            {
+                this.logoTween.pause();
+            }
+            else
+            {
+                this.logoTween.play();
+            }
+        } 
+        else
+        {
+            this.logoTween = this.tweens.add({
+                targets: this.logo,
+                x: { value: 750, duration: 3000, ease: 'Back.easeInOut' },
+                y: { value: 80, duration: 1500, ease: 'Sine.easeOut' },
+                yoyo: true,
+                repeat: -1,
+                onUpdate: () => {
+                    if (vueCallback)
+                    {
+                        vueCallback({
+                            x: Math.floor(this.logo.x),
+                            y: Math.floor(this.logo.y)
+                        });
+                    }
+                }
+            });
+        }
+    }
+}
